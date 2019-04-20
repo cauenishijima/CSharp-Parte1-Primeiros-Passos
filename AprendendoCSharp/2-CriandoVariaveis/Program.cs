964bee47fbcd50325b1d9aed1aaf49fd12cd8493
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-20 09:57:10 -0300

    Criando variável, atribuindo valores e utilizando a variável.

--- AprendendoCSharp/2-CriandoVariaveis/Program.cs
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -12,6 +12,23 @@ namespace _2_CriandoVariaveis
         {
             Console.WriteLine("Executando Projeto 2 - Criando Variáveis");
 
+            int idade;
+
+            idade = 32;
+            Console.WriteLine(idade);
+
+            idade = 10;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5;
+            Console.WriteLine(idade);
+
+            idade = 10 + 5 * 2;
+            Console.WriteLine(idade);
+
+            idade = (10 + 5) * 2;
+            Console.WriteLine("Sua idade é: " + idade);
+
             Console.WriteLine("Execução finalizada. Tecle enter para sair...");
             Console.ReadLine();
         }
