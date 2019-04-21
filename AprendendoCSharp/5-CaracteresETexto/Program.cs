c144dd4453b81f3b1539f91f4e3b89d8033c1167
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-21 12:20:00 -0300

    Trabalhando com caracteres e textos

---
+++ AprendendoCSharp/5-CaracteresETexto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 5 - Caracteres e Texto");
+
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura cursos de tecnologia";
+            string cursosProgramacao = @"- .NET
+- Java
+- JavaScript";
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.WriteLine("A execuçao terminou. Tecle enter para finalizar.");
+            Console.ReadLine();
+        }
+    }
+}
