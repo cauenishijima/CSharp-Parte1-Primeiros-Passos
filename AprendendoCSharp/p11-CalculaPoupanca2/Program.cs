b20704536cc5059b1904d4ff214d6a7a959f2a7b
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-21 13:19:07 -0300

    Calculo da Poupança utilizando laço de repetição FOR

---
+++ AprendendoCSharp/p11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p11_CalculaPoupanca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Calcula Poupança 2");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes=1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Após {0} mês, você terá R$ " + valorInvestido.ToString("F2"), contadorMes);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
