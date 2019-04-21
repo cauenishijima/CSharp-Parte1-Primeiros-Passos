96b595614f5afef81041d0aef978b1b0cbd18fd6
Cauê Nishijima <caue_nishijima@yahoo.com.br>
2019-04-21 13:28:15 -0300

    Investimento a longo prazo - FOR encadeado

---
+++ AprendendoCSharp/p12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p12_CalculaInvestimentoLongoPrazo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 12");
+
+            double valorInvestido = 1000;
+            double fatorRendimento = 1.0036;
+
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                    Console.WriteLine("O novo valor no mês {0} do ano {1} é {2}", contadorMes, contadorAno, valorInvestido.ToString("F2"));
+                }
+                fatorRendimento += 0.0010;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
